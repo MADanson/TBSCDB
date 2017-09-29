c626384ed304c7300fde7611412cb3adf693df93
Matthew Jennings <matthewadjennings@gmail.com>
2017-09-29 21:05:20 +0100

    Cock
    Faggot

--- DBSystemController/DBSystemController/Form1.Designer.cs
+++ DBSystemController/DBSystemController/Form1.Designer.cs
@@ -33,7 +33,7 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(157, 108);
+            this.button1.Location = new System.Drawing.Point(82, 59);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
@@ -44,7 +44,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(757, 524);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.ShowIcon = false;
